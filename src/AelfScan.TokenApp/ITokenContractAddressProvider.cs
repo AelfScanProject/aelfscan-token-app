aada361448082cf00bad55d5f529a7f59f24e448
joker.w <joker.w@aelfscan.io>
2024-05-07 19:38:24 +0800

    add init param

--- src/AelfScan.TokenApp/ITokenContractAddressProvider.cs
+++ src/AelfScan.TokenApp/ITokenContractAddressProvider.cs
@@ -8,12 +8,8 @@ public interface ITokenContractAddressProvider
 
 public class TokenContractAddressProvider : ITokenContractAddressProvider
 {
-    private readonly Dictionary<string, string> _contractAddresses = new()
-    {
-        { "AELF", "JRmBduh4nXWi1aXgdUsj5gJrzeZb2LxmrAbf7W99faZSvoAaE" },
-        { "tDVV", "7RzVGiuVWkvL4VfVHdZfQF2Tri3sgLe9U991bohHFfSRZXuGX" }
-    };
-
+    private readonly Dictionary<string, string> _contractAddresses = TokenAppConstants.ContractAddresses;
+    
     public string GetContractAddress(string chainId)
     {
         return _contractAddresses[chainId];
--- src/AelfScan.TokenApp/TokenAppConstants.cs
+++ src/AelfScan.TokenApp/TokenAppConstants.cs
@@ -2,6 +2,12 @@ namespace AElfScan.TokenApp;
 
 public class TokenAppConstants
 {
+    public static Dictionary<string, string> ContractAddresses = new()
+    {
+        { "AELF", "JRmBduh4nXWi1aXgdUsj5gJrzeZb2LxmrAbf7W99faZSvoAaE" },
+        { "tDVV", "7RzVGiuVWkvL4VfVHdZfQF2Tri3sgLe9U991bohHFfSRZXuGX" }
+    };
+    
     public static Dictionary<string,long> InitialBalanceEndHeight = new Dictionary<string, long>
     {
         { "AELF", 4100 },
